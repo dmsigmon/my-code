d4765118d31da846665ebce32640bea7ed741ac3
Douglas Sigmon <douglas.sigmon@gmail.com>
2024-08-08 02:24:14 +0000

    finished mini project

--- MiniProject/project.py
+++ MiniProject/project.py
@@ -1,3 +1,28 @@
 #!usr/bin/python3
+import random
 
+rps_array =["rock","paper", "scissors"]
+
+while True:
+    try:
+        player_choice = input("rock, paper, or scissors? \t")
+        computer_choice = random.choice(rps_array)
+
+        if player_choice in rps_array:
+            if player_choice == computer_choice:
+                print(f"you both chose {player_choice}, try again")
+            elif player_choice == "rock" and computer_choice == "scissors":
+                print(f"you win! you chose [{player_choice}] and computer chose [{computer_choice}]")
+                break
+            elif player_choice == "scissors" and computer_choice == "paper":
+                print(f"you win! you chose [{player_choice}] and computer chose [{computer_choice}]")
+                break 
+            elif player_choice == "paper" and computer_choice == "rock":
+                print(f"you win! you chose [{player_choice}] and computer chose [{computer_choice}]")
+                break
+            else:
+                print(f"Sorry you lost! you chose [{player_choice}] and computer chose [{computer_choice}]")
+                break
+    except Exception:
+        print("choose rock, paper, or scissors")
 
